80db16c84f6f4ffb7ef3718b46aaeb6e2bac27a2
iwanderer <iwanderer.lee@gmail.com>
2016-08-15 11:53:51 +0800

    Add SingleActivityFragment.java and its layout

---
+++ PhotoGallery/app/src/main/java/com/example/leslie/photogallery/SingleActivityFragment.java
@@ -0,0 +1,26 @@
+package com.iwanderer.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.*;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by leslie on 2016/8/8.
+ */
+public abstract class SingleActivityFragment extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+}
