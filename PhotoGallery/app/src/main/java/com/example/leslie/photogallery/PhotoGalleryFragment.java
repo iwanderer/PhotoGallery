07988c03d5cfffb3bb6cdaebd04779835e3ed7e7
iwanderer <iwanderer.lee@gmail.com>
2016-08-15 12:17:18 +0800

    Updated PhotoGalleryFragment

--- PhotoGallery/app/src/main/java/com/example/leslie/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/example/leslie/photogallery/PhotoGalleryFragment.java
@@ -1,10 +1,31 @@
 package com.example.leslie.photogallery;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
 
 /**
  * Created by leslie on 2016/8/15.
  */
 public class PhotoGalleryFragment extends Fragment {
+    private GridView mGridView;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mGridView = (GridView) v.findViewById(R.id.gridView);
+        return v;
+    }
 }
