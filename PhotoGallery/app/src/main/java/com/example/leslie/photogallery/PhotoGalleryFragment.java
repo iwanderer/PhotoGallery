60d94616deb268c5a0abe160f7e7ff02059fee2f
iwanderer <iwanderer.lee@gmail.com>
2016-08-23 11:28:01 +0800

    Add GridView to PhotoGalleryFragment

--- PhotoGallery/app/src/main/java/com/example/leslie/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/example/leslie/photogallery/PhotoGalleryFragment.java
@@ -8,9 +8,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
-import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by leslie on 2016/8/15.
@@ -18,6 +19,7 @@ import java.io.IOException;
 public class PhotoGalleryFragment extends Fragment {
     private static final String TAG = "PhotoGalleryFragment";
     private GridView mGridView;
+    private ArrayList<GalleryItem> mItems;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,15 +35,27 @@ public class PhotoGalleryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
         mGridView = (GridView) v.findViewById(R.id.gridView);
+        setupAdapter();
         return v;
     }
 
-    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+    private void setupAdapter() {
+        if (getActivity() == null || mGridView == null) return;
+
+        if (mItems != null) {
+            mGridView.setAdapter(new ArrayAdapter<>(getActivity(), android.R.layout.simple_gallery_item, mItems));
+        } else {
+            mGridView.setAdapter(null);
+        }
+    }
+
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, ArrayList<GalleryItem>> {
 
         @Override
-        protected Void doInBackground(Void... voids) {
-            new FlickrFetchr().fetchItems();
-            return null;
+        protected ArrayList<GalleryItem> doInBackground(Void... voids) {
+            mItems = new FlickrFetchr().fetchItems();
+            return mItems;
         }
     }
 }
